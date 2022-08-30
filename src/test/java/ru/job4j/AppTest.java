c018eb0ad3f58746e6739b28243c590ac6b41101
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-08-30 12:01:21 +0700

    fix checkstyle

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -10,31 +10,27 @@ import static org.assertj.core.api.Assertions.*;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return (Test) new TestSuite( AppTest.class );
+    public static Test suite() {
+        return (Test) new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
