51ca24fee0b4815135b52eb081ea0fd87d7cdc9d
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-08-30 11:03:46 +0700

    Подключение библиотеки AssertJ [#504881]

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -1,8 +1,10 @@
 package ru.job4j;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Unit test for simple App.
@@ -25,7 +27,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return (Test) new TestSuite( AppTest.class );
     }
 
     /**
