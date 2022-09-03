25f0c4db628e0ec0a5a56d555ca93a4e4f559f85
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-09-03 23:18:41 +0700

    7. ListIterator [#350217]

---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.iterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void ifElemEqual3ThenSizeIs1() {
+        Predicate<Integer> pred = el -> el == 3;
+        ListUtils.removeIf(input, pred);
+        assertThat(input).hasSize(1)
+                .containsSequence(1);
+    }
+
+    @Test
+    void ifElementsIs13WhenListSizeIs0() {
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 3));
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(1, 3, 2, 6, 7));
+        ListUtils.removeAll(inputList, elements);
+        assertThat(inputList).containsSequence(2, 6, 7);
+    }
+}
