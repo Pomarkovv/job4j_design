322d52a0be2c37c0f6adc83200edf629f23e38d1
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-09-14 12:48:08 +0700

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            Boolean errorOnServer = true;
+            while (read.ready()) {
+                String line = read.readLine();
+                if (errorOnServer && (line.startsWith("400") || line.startsWith("500"))) {
+                    out.write(line.split(" ")[1] + ";");
+                    errorOnServer = false;
+                } else if (!errorOnServer && !line.startsWith("400") && !line.startsWith("500")) {
+                    out.write(line.split(" ")[1] + System.lineSeparator());
+                    errorOnServer = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("./data/sourceLog", "./data/errLogs");
+    }
+}
