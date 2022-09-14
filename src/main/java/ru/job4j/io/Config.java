e55711dac66e78747f0a01b1fc5dbb894ff3b9e4
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-09-14 11:07:03 +0700

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.path))) {
+            while (bufferedReader.ready()) {
+                String readLine = bufferedReader.readLine();
+                if (!readLine.startsWith("#") && readLine.length() > 0) {
+                    String[] lines = readLine.split("=", 2);
+                    if (lines.length != 2 || readLine.startsWith("=")) {
+                        throw new IllegalArgumentException();
+                    }
+                    values.put(lines[0], lines[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("./data/pair_without_comment.properties"));
+        System.out.println();
+    }
+
+}
