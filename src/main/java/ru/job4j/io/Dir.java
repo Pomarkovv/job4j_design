30e2ac5581ad4891d48d616eed7ce3abf87ba8cd
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-09-15 11:33:01 +0700

    fix in Dir

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -11,9 +11,9 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("Total disk size : %s Gb", file.getTotalSpace() / 1073741824));
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
+            System.out.printf("name: %s size: %s %n", subfile.getName(), subfile.length());
         }
     }
 }
