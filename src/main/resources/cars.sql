175bce430f6eb0655e549145afb3164201f9ad15
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-09-29 08:40:39 +0700

    Хранилище машин.

--- src/main/resources/cars.sql
+++ src/main/resources/cars.sql
@@ -25,7 +25,9 @@ insert into car_bodies(name) values ('1X1234'), ('1Y2341'), ('3V2321'), ('2C3421
 insert into car_engines(name) values ('JZX-6.5'), ('V8-7.2'), ('Niss-1.3'), ('UrusEng12');
 insert into car_transmissions(name) values ('ertyu'), ('asdfg'), ('putinVor'), ('Uzxcvg');
 
-insert into cars(name, body_id, eng_id, trans_id) values ('Nissan AD', 1, 1, 1), ('LandCruiser200', 2, 2, 2), ('Lambo', 3, 3, 3);
+insert into cars(name, body_id, eng_id, trans_id)
+values ('Nissan AD', 1, 1, 1), ('LandCruiser200', 2, 2, 2), ('Lambo', 3, 3, 3);
+
 insert into cars(name, body_id, trans_id) values ('Lada', 4, 4);
 
 select c.name car, b.name body, e.name engine, t.name transmission from cars c
@@ -35,5 +37,5 @@ left join car_transmissions t on c.trans_id = t.id
 order by c.name;
 
 select b.name from car_bodies b left join cars c on c.body_id = b.id where c.id is null;
-select e.name from car_emgines e left join cars c on c.eng_id = e.id where c.id is null;
+select e.name from car_engines e left join cars c on c.eng_id = e.id where c.id is null;
 select t.name from car_transmissions t left join cars c on c.trans_id = t.id where c.id is null;
